fa0cef8b0e62dca229610c1abf210809b825371a
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-08-26 21:26:28 +0900

    タスクの作成と表示

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,53 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
+type Todo struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	Title string `json:"title"`
+	Done bool `json:"done"`
+}
+
+var db *gorm.DB
 
 func main() {
-	r := gin.Default()
+	var err error
+	db, err = gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello World")
-	})
+	db.AutoMigrate(&Todo{})
+
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/todos", getTodos)
+
+	r.POST("/todos", createTodo)
 
 	r.Run(":8080")
+}
+
+// GET /todos
+func getTodos(c *gin.Context) {
+	var todos []Todo
+	db.Find(&todos)
+	c.JSON(200, todos)
+}
+
+// POST /todos
+func createTodo(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&todo)
+	c.JSON(200, todo)
 }
